9f7eed4c8cc198a9a150a9178a6361ee71ea08b5
whooki3 <nickgszabo@protonmail.com>
2023-05-05 17:36:35 +0200

    added to do

---
+++ src/TODO.h
@@ -0,0 +1,33 @@
+#pragma once
+
+/// <TO DO>
+/// Add the option to turn on or off a cell
+///
+/// Adding 'long long' to shader with the right extension on the Vulkan side
+///
+/// Adding screenspace position to vertex ID, to click on objects and return
+/// their IDs
+///
+/// Adding vertices and indices to instance cubes instead of grid points
+/// Adding model, view, projection matrices
+///
+/// Adding camera movement connected to the mouse
+///
+/// Moving one cell to another, than replacing the moving cell with another
+/// object (adding trading rules, trade with best neightbour, richest, furthest,
+/// closest)
+///
+/// Add multisampling
+///
+/// Add depth stenciling
+///
+/// Add other objects on same grid
+///
+/// Add lightning (point lightning)
+///
+/// Add other geometry (landscape grid + y height, and other objects)
+///		- Add landownership vinroy effect (camera that films cones that
+///		  overlap, as texture on landscape)
+///
+///
+/// </TO DO>
