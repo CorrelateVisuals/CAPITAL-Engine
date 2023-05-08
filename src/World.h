367179d8787592c25902b4a986d41287d295ae32
whooki3 <nickgszabo@protonmail.com>
2023-05-08 20:12:55 +0200

    removed cube vertices

--- src/World.h
+++ src/World.h
@@ -29,15 +29,6 @@ class World {
     std::array<float, 4> alive;
   };
 
-  inline static const std::vector<Cell> cubeVertices = {
-      {-1.0f, -1.0f, -1.0f}, {1.0f, -1.0f, -1.0f}, {-1.0f, 1.0f, -1.0f},
-      {1.0f, 1.0f, -1.0f},   {-1.0f, -1.0f, 1.0f}, {1.0f, -1.0f, 1.0f},
-      {-1.0f, 1.0f, 1.0f},   {1.0f, 1.0f, 1.0f}};
-  inline static const std::vector<uint16_t> cubeIndices = {
-      0, 2, 3, 0, 3, 1, 4, 5, 7, 4, 7, 6, 1, 3, 7, 1, 7, 5,
-      0, 4, 6, 0, 6, 2, 2, 6, 7, 2, 7, 3, 0, 1, 5, 0, 5, 4,
-  };
-
  public:
   std::vector<int> setCellsAliveRandomly(int size);
   static std::vector<VkVertexInputAttributeDescription>
