a561f388052c0854ee3a6a065820cae55f64546b
whooki3 <nickgszabo@protonmail.com>
2023-05-12 12:42:39 +0200

    Update TODO.h

--- src/TODO.h
+++ src/TODO.h
@@ -10,14 +10,17 @@
 /// - Remove the vertex, index and normal data from vertex shader to c++
 ///
 /// FEATURES: ADDITIONS
-/// - Add Dear ImGui
-/// - Add multisampling
-/// - Adding screenspace position to vertex ID, to click on objects and return
-/// their IDs (renderpicking). A Vulkan attachment is needed for this.
+/// - Dear ImGui
+/// - Multisampling
+/// - Renderpicking: Adding screenspace position to vertex ID, to click on
+/// objects and return their IDs. A Vulkan attachment is needed for this
+/// - Low Frequency Oscilator. That animates a value smoothly between a
+/// range, based on a speed. Settings like lfo, ramp, step etc are usefull too
 ///
 /// FEATURES: GEOMETRY
-/// - Moving geometry between cells
-/// - Add base terrain (landscape grid with height)
+/// - Animating geometry between cells and implementing Touchdesigner fragment
+/// shader proof of concept in the compute shader
+/// - Terrain (needs new pipeline)
 ///		- Add landownership vinroy effect (camera that films cones that
 ///		  overlap, as texture on landscape)
 ///
