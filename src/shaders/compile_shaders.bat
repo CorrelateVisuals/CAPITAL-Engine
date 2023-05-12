567b58bb512043c1e995b6a6b16c349e74d2fa5f
whooki3 <nickgszabo@protonmail.com>
2023-05-12 19:32:48 +0200

    naming conventions

--- src/shaders/compile_shaders.bat
+++ src/shaders/compile_shaders.bat
@@ -1,4 +1,4 @@
-C:/VulkanSDK/1.3.224.1/Bin/glslangValidator.exe -V shader.vert
-C:/VulkanSDK/1.3.224.1/Bin/glslangValidator.exe -V shader.frag
-C:/VulkanSDK/1.3.224.1/Bin/glslangValidator.exe -V shader.comp
+glslangValidator.exe -V shader.vert
+glslangValidator.exe -V shader.frag
+glslangValidator.exe -V shader.comp
 pause
