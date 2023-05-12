2e21625bf3bad280676b19e4cf69051ebf73ef9d
whooki3 <nickgszabo@protonmail.com>
2023-05-12 21:23:40 +0200

    updated to work on windows

--- shaders/compile_shaders.bat
+++ shaders/compile_shaders.bat
@@ -1,4 +1,4 @@
-glslangValidator.exe -V shader.vert
-glslangValidator.exe -V shader.frag
-glslangValidator.exe -V shader.comp
+glslangValidator.exe -V shader.vert -o ..\src\shaders\vert.spv
+glslangValidator.exe -V shader.frag -o ..\src\shaders\frag.spv
+glslangValidator.exe -V shader.comp -o ..\src\shaders\comp.spv
 pause
