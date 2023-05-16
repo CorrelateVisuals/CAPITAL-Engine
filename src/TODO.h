9df6608353087e2e365b3608df35ad2b9366e36f
whooki3 <nickgszabo@protonmail.com>
2023-05-16 20:45:02 +0200

    Update TODO.h

--- src/TODO.h
+++ src/TODO.h
@@ -9,10 +9,9 @@
 /// on the Vulkan side
 /// - Correct the scaling of the compute row in relation to the grid size.
 /// Currently sqrt() of one of the grid dimensions is used to do so and manually
-/// this value has to be set in the compute shader
+/// this value has to be set in the compute shader, could it be dynamic?
 /// - Remove the vertex, index and normal data from vertex shader to c++
 /// - Optimization and bug fixes
-/// - Ensure WINDOWS and LINUX compatibility
 /// - Revise the _log.console() function (with the use of overload functions)
 /// - Format runtime errors to right style format
 ///
@@ -21,8 +20,6 @@
 /// - Multisampling
 /// - Renderpicking: Adding screenspace position to vertex ID, to click on
 /// objects and return their IDs. A Vulkan attachment is needed for this
-/// - Low Frequency Oscilator. That animates a value smoothly between a
-/// range, based on a speed. Settings like lfo, ramp, step etc are usefull too
 ///
 /// [ FEATURES: GEOMETRY ]
 /// - Animating geometry between cells and implementing Touchdesigner fragment
@@ -30,6 +27,5 @@
 /// - Terrain (needs new pipeline)
 ///		- Add landownership vinroy effect (camera that films cones that
 ///		  overlap, as texture on landscape)
-///
 ///
 /// </TO DO> ///////////////////////////////////////////////////////////////////
