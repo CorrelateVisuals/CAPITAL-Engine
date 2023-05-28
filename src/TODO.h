4696d32d7cc9a55a0d6e127e066d07798d13cd23
whooki3 <jakobpovel@gmail.com>
2023-05-28 23:37:41 +0200

    Update TODO.h

--- src/TODO.h
+++ src/TODO.h
@@ -2,28 +2,26 @@
 /// <TO DO> ///////////////////////////////////////////////////////////////////
 ///
 /// [ FIND BUGS ]
-/// - Check if functions work how they are supposed to work
+/// - Check if functions work as they are supposed to work
 ///
 /// [ BUGS ]
 /// - Adding 'long long' data type to shader, with enabling the right extension
 /// on the Vulkan side
-/// - Remove the vertex, index and normal data from vertex shader to c++
-/// - Optimization and bug fixes
+/// - Add vertex and index buffers; remove the vertex, index and normal data
+/// from vertex shader
 /// - Revise the _log.console() function (with the use of overload functions)
-/// - Format runtime errors to right style format
 /// - Adding CPU access to cell Shader data through shaderStorage buffer
 ///
 /// [ FEATURES: ADDITIONS ]
 /// - Dear ImGui (for runtime settings, graph data and economic encyclopedia)
 /// - Multisampling
-/// - Renderpicking: Adding screenspace position to vertex ID, to click on
-/// objects and return their IDs. A Vulkan attachment is needed for this
-/// - Ray-tracing
+/// - Renderpicking/colorpicking: Adding screenspace position to vertex ID, to
+/// click on objects and return their IDs.
+/// - Add camera movements, arcball or common
 ///
 /// [ FEATURES: GEOMETRY ]
-/// - Animating geometry between cells and implementing Touchdesigner fragment
-/// shader proof of concept in the compute shader
-/// - Terrain (needs new pipeline)
+/// - Animating geometry between cells
+/// - Terrain (needs new pipeline?)
 ///		- Add landownership vinroy effect (camera that films cones that
 ///		  overlap, as texture on landscape)
 ///
