af62df5dbfd15348215bda65209232f4b90b2cc1
whooki3 <jakobpovel@gmail.com>
2023-05-30 22:15:01 +0200

    Update Window.h

--- src/Window.h
+++ src/Window.h
@@ -12,10 +12,10 @@ class Window {
   bool framebufferResized;
 
   struct Mouse {
-    float pressDelayDuration = 0.12f;
-    std::array<double, 2> leftCoords = {parameterOff, parameterOff};
-    std::array<double, 2> middleCoords = {parameterOff, parameterOff};
-    std::array<double, 2> rightCoords = {parameterOff, parameterOff};
+    float pressDelayDuration = 0.18f;
+    std::array<float, 2> leftCoords = {parameterOff, parameterOff};
+    std::array<float, 2> middleCoords = {parameterOff, parameterOff};
+    std::array<float, 2> rightCoords = {parameterOff, parameterOff};
   } mouse;
 
   void mouseClick(GLFWwindow* win);
