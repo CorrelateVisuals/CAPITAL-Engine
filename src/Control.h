6cceb65749979a7e959a1ebd4311bd0794eeced2
whooki3 <jakobpovel@gmail.com>
2023-06-21 03:15:38 +0200

    full houaw

--- src/Control.h
+++ src/Control.h
@@ -19,8 +19,8 @@ class Control {
   struct Grid {
     uint_fast32_t totalAliveCells = 1250;
     std::array<uint_fast16_t, 2> dimensions = {50, 50};
-    float gap = 0.75f;
-    float height = 0.5f;
+    float gap = 0.6f;
+    float height = 0.0f;
   } grid;
 
   struct DisplayConfiguration {
--- src/World.h
+++ src/World.h
@@ -44,7 +44,7 @@ class World {
   };
 
   struct Cube {
-    uint32_t vertexCount = 66;
+    uint32_t vertexCount = 84;
     float size = 0.1f;
   } cube;
 
