a0166f6f7d2d8a2109b1913d1f0739d008c4557e
whooki3 <jakobpovel@gmail.com>
2023-08-01 16:34:42 +0200

    Update TODO.h

--- src/TODO.h
+++ src/TODO.h
@@ -2,23 +2,21 @@
 /// <TO DO> //////////////////////////////////////////////////////////////////
 ///
 /// [ BUGS ]
-/// - tile corner vertex calculation to vertex shader
-/// - distant vertices jitter
+/// - distant vertex/surface jitter
 ///
 /// [ FEATURES: ADDITIONS ]
 /// - camera arcball camera
 /// https://github.com/Twinklebear/arcball-cpp
 /// - Dear ImGui
 /// https://github.com/ocornut/imgui
-/// - colorpicking; screenspace position to vertex ID
+/// - Colorpicking; screenspace position to vertex ID
 /// https://snoozetime.github.io/2019/05/02/object-picking.html
-/// - vertex and index buffers
+/// - Vertex and index buffers
 /// https://vulkan-tutorial.com/Vertex_buffers/Vertex_buffer_creation
-/// - compute cull and lod
-/// - 2Dsampler
+/// - Compute cull and lod
+/// - 2Dsampler to read GPU written textures from the CPU
 ///
 /// [ FEATURES: GEOMETRY ]
 /// - inter cell movement
-///	- vinroy effect
 ///
 /// </TO DO> ///////////////////////////////////////////////////////////////////
